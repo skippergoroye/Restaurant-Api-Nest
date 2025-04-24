c0f5b50ee59c41dc6e7f62ecc441cc966b2a20a1
skippergoroye <oluwatosinadegoroye21@gmail.com>
2025-04-24 15:48:55 +0100

    feat: schemacreated

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { RestaurantsModule } from './restaurants/restaurants.module';
+
 
 @Module({
-  imports: [],
+  imports: [ ConfigModule.forRoot({
+    envFilePath: ".env",
+    isGlobal: true
+  }),
+  MongooseModule.forRoot(process.env.DB_URI || 'mongodb://localhost:27017/library-nest-api'),RestaurantsModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
+  const port = process.env.PORT || 3000;
+  app.enableCors({
+    origin: 'http://localhost:3000', 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+  console.log(`Server running in  on port ${port}`);
 }
 bootstrap();
