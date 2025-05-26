c284a68878b4b135926bb5c2b9f89e96fe3baf6e
skippergoroye <oluwatosinadegoroye21@gmail.com>
2025-05-26 08:37:41 +0100

    feat: file upload added for single and multiple images

--- src/restaurants/restaurants.controller.ts
+++ src/restaurants/restaurants.controller.ts
@@ -1,51 +1,81 @@
-import { Body, Controller, Delete, Get, Param, Post, Put, Query } from '@nestjs/common';
-import {  Query as ExpressQuery } from "express-serve-static-core"
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+  UploadedFile,
+  UploadedFiles,
+  UseInterceptors,
+} from '@nestjs/common';
+import { Query as ExpressQuery } from 'express-serve-static-core';
 import { Restaurant } from './schemas/restaurant.schema';
 import { RestaurantsService } from './restaurants.service';
 import { createRestaurantDto } from './dto/create-restaurant.dto';
 import { updateRestaurantDto } from './dto/update-restaurant.dto';
+import { FilesInterceptor } from '@nestjs/platform-express';
 
 @Controller('restaurants')
 export class RestaurantsController {
-    constructor(private restaurantsService: RestaurantsService) {}
+  constructor(private restaurantsService: RestaurantsService) {}
 
+  @Get()
+  async getAllRestaurants(@Query() query: ExpressQuery): Promise<Restaurant[]> {
+    return await this.restaurantsService.findAll(query);
+  }
 
-    @Get()
-    async getAllRestaurants(@Query() query:ExpressQuery  ): Promise<Restaurant[]> {
-        return await this.restaurantsService .findAll(query);
+  @Post()
+  async createRestaurant(
+    @Body() restaurant: createRestaurantDto,
+  ): Promise<Restaurant> {
+    return await this.restaurantsService.create(restaurant);
+  }
 
+  @Get(':id')
+  async getRestaurant(@Param('id') id: string): Promise<Restaurant> {
+    return await this.restaurantsService.findById(id);
+  }
+
+  @Put(':id')
+  async updateRestaurant(
+    @Param('id') id: string,
+    @Body() restaurant: updateRestaurantDto,
+  ): Promise<Restaurant> {
+    return await this.restaurantsService.updateById(id, restaurant);
+  }
+
+  @Delete(':id')
+  async deleteRestaurant(
+    @Param('id') id: string,
+  ): Promise<{ deleted: boolean }> {
+    const restaurant = await this.restaurantsService.deleteById(id);
+
+    if (restaurant) {
+      return { deleted: true };
     }
 
+    return { deleted: false };
+  }
 
-    @Post()
-    async createRestaurant(@Body() restaurant: createRestaurantDto): Promise<Restaurant> {
-        return await this.restaurantsService.create(restaurant);
-    }
+  // works for single file upload  (FileInterceptor) (UploadedFile) file
+  //   @Put('upload/:id')
+  //   @UseInterceptors(FileInterceptor('file'))
+  //   async uploadFiles(@Param('id') id: string, @UploadedFile() files: Array<Express.Multer.File>) {
+  //     console.log(id)
+  //     console.log(files)
+  //   }
 
-
-    @Get(':id')
-    async getRestaurant( @Param('id') id: string): Promise<Restaurant> {
-        return await this.restaurantsService.findById (id);
-    }
-
-
-
-    @Put(':id')
-    async updateRestaurant( @Param('id') id: string, @Body() restaurant: updateRestaurantDto ): Promise<Restaurant> {
-        return await this.restaurantsService.updateById (id, restaurant);
-    }
-
-
-    @Delete(':id')
-    async deleteRestaurant( @Param('id') id: string): Promise<{deleted: boolean}> {
-
-        const restaurant = await this.restaurantsService.deleteById(id);
-        
-
-        if(restaurant) {
-            return { deleted: true };
-        }
-
-        return { deleted: false };
-    }
+  // multiple file upload
+  @Put('upload/:id')
+  @UseInterceptors(FilesInterceptor('files'))
+  async uploadFiles(
+    @Param('id') id: string,
+    @UploadedFiles() files: Array<Express.Multer.File>,
+  ) {
+    console.log(id);
+    console.log(files);
+  }
 }
